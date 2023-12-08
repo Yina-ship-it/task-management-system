e183250a7a2aa9da2b0198c1137441617a934cae
Yina_ship_it <najdenov7731@gmail.com>
2023-12-09 02:09:28 +0500

    created dtos AuthRequest, AuthResponse, RegistrationRequest

---
+++ src/main/java/com/example/taskmanagementsystem/security/dto/AuthRequest.java
@@ -0,0 +1,20 @@
+package com.example.taskmanagementsystem.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Yina-ship-it
+ * @since 08.12.2023
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+    private String email;
+    private String password;
+}
