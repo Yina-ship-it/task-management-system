afa4c74f09d49b79ba602ba11dca464655967bc4
Yina_ship_it <najdenov7731@gmail.com>
2023-12-09 19:29:08 +0500

    create UserResponse

---
+++ src/main/java/com/example/taskmanagementsystem/dto/profile/UserResponse.java
@@ -0,0 +1,19 @@
+package com.example.taskmanagementsystem.dto.profile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Yina-ship-it
+ * @since 09.12.2023
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+    private String name;
+    private String email;
+}
