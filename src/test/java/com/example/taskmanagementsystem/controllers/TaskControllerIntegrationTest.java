38a16bd2b910649f9056c85a498a88c2de00cc9a
Yina_ship_it <najdenov7731@gmail.com>
2023-12-10 13:07:52 +0500

    refactoring in TaskControllerIntegrationTest

--- src/test/java/com/example/taskmanagementsystem/controllers/TaskControllerIntegrationTest.java
+++ src/test/java/com/example/taskmanagementsystem/controllers/TaskControllerIntegrationTest.java
@@ -64,22 +64,29 @@ class TaskControllerIntegrationTest {
 
     private String token;
     private List<User> users;
+    private List<Task> tasks;
+    private TaskRequest taskRequest;
+
 
     @BeforeEach
     void setUp() {
+        this.users = createUsers();
+        this.token = jwtProvider.generateToken(users.get(0).getEmail());
+        this.tasks = createTasks();
+        this.taskRequest = createTaskRequest();
+    }
+
+    private List<User> createUsers(){
         String password = passwordEncoder.encode("Password");
-        this.users = userRepository.saveAll(List.of(
+        return userRepository.saveAll(List.of(
                 User.builder().name("maksim1").email("maksim1@mail.test").password(password).build(),
                 User.builder().name("maksim2").email("maksim2@mail.test").password(password).build(),
                 User.builder().name("maksim3").email("maksim3@mail.test").password(password).build()
         ));
-        this.token = jwtProvider.generateToken(users.get(0).getEmail());
     }
 
-    @Test
-    void getAllTasks_ShouldReturnListOfTaskResponses() throws Exception {
-        // Arrange
-        List<Task> tasks = taskRepository.saveAll(List.of(
+    private List<Task> createTasks(){
+        return taskRepository.saveAll(List.of(
                 Task.builder()
                         .title("TestTask1")
                         .description("task 1")
@@ -97,6 +104,22 @@ class TaskControllerIntegrationTest {
                         .assignees(Set.of(users.get(0), users.get(2)))
                         .build()
         ));
+    }
+
+    private TaskRequest createTaskRequest(){
+        return TaskRequest.builder()
+                .title("TestTask")
+                .description("testing task")
+                .statusValue(1)
+                .priorityValue(2)
+                .assigneesEmail(List.of(users.get(1).getEmail()))
+                .assigneesId(List.of(users.get(2).getId()))
+                .build();
+    }
+
+    @Test
+    void getAllTasks_ShouldReturnListOfTaskResponses() throws Exception {
+        // Arrange
         List<TaskResponse> taskResponses = tasks.stream()
                 .map(taskDtoConverter::convertEntityToDto)
                 .map(taskDtoConverter::convertDtoToResponse)
@@ -124,18 +147,10 @@ class TaskControllerIntegrationTest {
     @Test
     void getTaskById_WhenTaskFound_ShouldTaskResponse() throws Exception {
         // Arrange
-        Task task = taskRepository.save(Task.builder()
-                        .title("TestTask1")
-                        .description("task 1")
-                        .priority(TaskPriority.MEDIUM)
-                        .status(TaskStatus.IN_PROGRESS)
-                        .author(users.get(0))
-                        .assignees(Set.of(users.get(1), users.get(2)))
-                        .build());
-        TaskResponse taskResponse = taskDtoConverter.convertDtoToResponse(taskDtoConverter.convertEntityToDto(task));
+        TaskResponse taskResponse = taskDtoConverter.convertDtoToResponse(taskDtoConverter.convertEntityToDto(tasks.get(0)));
 
         // Act
-        mockMvc.perform(get("/api/tasks/{id}", task.getId())
+        mockMvc.perform(get("/api/tasks/{id}", tasks.get(0).getId())
                         .header("Authorization", "Bearer " + token))
                 // Assert
                 .andExpect(status().isOk())
@@ -159,25 +174,13 @@ class TaskControllerIntegrationTest {
     @Test
     void getTaskById_UnauthorisedRequest_ShouldReturnForbiddenStatus() throws Exception {
         // Act
-        long id = Long.MAX_VALUE;
-
-        mockMvc.perform(get("/api/tasks/{id}", id))
+        mockMvc.perform(get("/api/tasks/{id}", tasks.get(0)))
                 // Assert
                 .andExpect(status().isForbidden());
     }
 
     @Test
     void addTask_WhenTaskRequestWithValidData_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
-        // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
-
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
                         .header("Authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON)
@@ -206,15 +209,6 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_UnauthorisedRequest_ShouldReturnForbiddenStatus() throws Exception {
         // Act
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
-
         mockMvc.perform(post("/api/tasks/")
                         .content(objectMapper.writeValueAsString(taskRequest)))
                 // Assert
@@ -224,13 +218,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutTitle_ShouldReturnBadRequestStatus() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setTitle(null);
 
         // Act
         mockMvc.perform(post("/api/tasks/")
@@ -243,14 +231,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithBlankTitle_ShouldReturnBadRequestStatus() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setTitle("");
 
         // Act
         mockMvc.perform(post("/api/tasks/")
@@ -263,13 +244,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutDescription_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setDescription(null);
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -299,14 +274,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithInvalidStatus_ShouldReturnBadRequestStatus() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(5)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setStatusValue(5);
 
         // Act
         mockMvc.perform(post("/api/tasks/")
@@ -319,13 +287,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutStatus_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setStatusValue(null);
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -355,14 +317,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithInvalidPriority_ShouldReturnBadRequestStatus() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(5)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setPriorityValue(5);
 
         // Act
         mockMvc.perform(post("/api/tasks/")
@@ -375,13 +330,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutPriority_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setPriorityValue(null);
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -411,13 +360,8 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutAssigneesEmail_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesId(List.of(users.get(1).getId(), users.get(2).getId()))
-                .build();
+        taskRequest.setAssigneesEmail(null);
+        taskRequest.setAssigneesId(List.of(users.get(1).getId(), users.get(2).getId()));
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -447,13 +391,8 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutAssigneesId_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail(), users.get(2).getEmail()))
-                .build();
+        taskRequest.setAssigneesEmail(List.of(users.get(1).getEmail(), users.get(2).getEmail()));
+        taskRequest.setAssigneesId(null);
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -483,14 +422,8 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenSameUsersSpecifiedInAssigneesIdAndAssigneesEmail_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesId(List.of(users.get(1).getId(), users.get(2).getId()))
-                .assigneesEmail(List.of(users.get(1).getEmail(), users.get(2).getEmail()))
-                .build();
+        taskRequest.setAssigneesId(List.of(users.get(1).getId(), users.get(2).getId()));
+        taskRequest.setAssigneesEmail(List.of(users.get(1).getEmail(), users.get(2).getEmail()));
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -520,14 +453,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenAssigneesIdContainsNonExistentUser_ShouldReturnNotFoundStatus() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail()))
-                .assigneesId(List.of(users.get(2).getId(), Long.MAX_VALUE))
-                .build();
+        taskRequest.setAssigneesId(List.of(users.get(2).getId(), Long.MAX_VALUE));
 
         // Act
         mockMvc.perform(post("/api/tasks/")
@@ -540,14 +466,7 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenAssigneesEmailContainsNonExistentUser_ShouldReturnNotFoundStatus() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .assigneesEmail(List.of(users.get(1).getEmail(), "TEst NonExisten USer Email"))
-                .assigneesId(List.of(users.get(2).getId()))
-                .build();
+        taskRequest.setAssigneesEmail(List.of(users.get(1).getEmail(), "TEst NonExistent USer Email"));
 
         // Act
         mockMvc.perform(post("/api/tasks/")
@@ -560,12 +479,8 @@ class TaskControllerIntegrationTest {
     @Test
     void addTask_WhenTaskRequestWithoutAssignees_ShouldReturnCreatedStatusAndTaskResponse() throws Exception {
         // Arrange
-        TaskRequest taskRequest = TaskRequest.builder()
-                .title("TestTask")
-                .description("testing task")
-                .statusValue(1)
-                .priorityValue(2)
-                .build();
+        taskRequest.setAssigneesEmail(null);
+        taskRequest.setAssigneesId(null);
 
         // Act
         String responseContent  = mockMvc.perform(post("/api/tasks/")
@@ -589,5 +504,4 @@ class TaskControllerIntegrationTest {
         assertEquals(users.get(0), createdTask.get().getAuthor());
         assertEquals(0, createdTask.get().getAssignees().size());
     }
-
 }
