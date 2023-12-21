d91f031fc4874bc32622817ee01a93157106751e
Yina_ship_it <najdenov7731@gmail.com>
2023-12-21 12:04:26 +0500

    added AutoConfigureTestDatabase

--- src/test/java/com/example/taskmanagementsystem/controllers/UserControllerIntegrationTest.java
+++ src/test/java/com/example/taskmanagementsystem/controllers/UserControllerIntegrationTest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -42,6 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 @Transactional
 class UserControllerIntegrationTest {
 
@@ -78,6 +80,7 @@ class UserControllerIntegrationTest {
 
     @BeforeEach
     void setUp() {
+        userRepository.deleteAll();
         this.users = createUsers();
         this.token = jwtProvider.generateToken(users.get(0).getEmail());
         this.tasks = createTasks();
