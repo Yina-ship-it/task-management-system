d0a42382032606a12ad8b83417482e654cff4ba9
Yina_ship_it <najdenov7731@gmail.com>
2023-12-21 12:06:55 +0500

    added creating default user

--- src/main/java/com/example/taskmanagementsystem/TaskManagementSystemApplication.java
+++ src/main/java/com/example/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -1,7 +1,12 @@
 package com.example.taskmanagementsystem;
 
+import com.example.taskmanagementsystem.models.User;
+import com.example.taskmanagementsystem.security.JwtProvider;
+import com.example.taskmanagementsystem.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskManagementSystemApplication {
@@ -10,4 +15,21 @@ public class TaskManagementSystemApplication {
 		SpringApplication.run(TaskManagementSystemApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService userService,
+			JwtProvider jwtProvider
+	) {
+		return args -> {
+			User user = User.builder()
+					.email("default_user@mail.test")
+					.password("defoult-user-password")
+					.name("Default User")
+					.build();
+			try {
+				userService.saveUser(user);
+			} catch (Exception ignored){}
+			System.out.println("Demo user token: " + jwtProvider.generateToken(user.getEmail()));
+		};
+	}
 }
